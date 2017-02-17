d519636026b8ca2ae92581d25cc7164c327175cf
Andrew Bell <andy.bell@informationage.co.nz>
2017-02-17 18:09:48 +1300

    Add basic tests and documentation

--- readme.md
+++ readme.md
@@ -1,3 +1,38 @@
 # mtype-node
 
-Object factory with schema validation for default tree nodes used by mojule
+Object factory with schema validation for this default tree nodes (entityNodes) used in [mojule](https://github.com/orgs/mojule/dashboard)
+
+Refer to :
+
+* Dependencies
+  * [mtype](https://github.com/mojule/mtype)
+  * [mtype-tv4](https://github.com/mojule/mtype-tv4)
+
+* Project json schema for mojule entityNode structure.
+  * emptyNode.schema.json
+  * entityNode.schema.json
+  * entityNodeValue.schema.json
+  * parentNode.schema.json
+
+## Usage
+
+
+
+```javascript
+
+const EntityNode = require( 'mtype-node' )
+
+// Factory returns instance of EntityNode
+const sampleNode = EntityNode('someNodeType')
+console.log( JSON.stringify( sampleNode ) ) // {"value":{"nodeType":"someNodeType","_id":"somenodetype-293c865aaf08ee5ab23eb335e9236908"},"children":[]}
+
+// Value factory returns instance of EntityNodeValue
+const sampleNodeValue = EntityNode.Value('someNodeType')
+console.log( JSON.stringify( sampleNodeValue ) ) // {"nodeType":"someNodeType","_id":"somenodetype-5a336a3c20029b79898900cae8700d75"}
+
+// Reference underlying instance of mtype
+const mtype = EntityNode.mtype
+console.log( types ) //[ 'entityNode', 'entityNodeValue', 'emptyNode', 'parentNode' ]
+
+```
+
--- test/index.js
+++ test/index.js
@@ -1,15 +1,47 @@
-"use strict"
+'use strict'
 
 const assert = require( 'assert' )
 const Node = require( '../' )
 
 describe( 'mtype-node', () => {
-  it( 'accepts valid node', () => {
-    const node = Node( 'someNodeType' )
-
-    assert.equal( node.value.nodeType, 'someNodeType' )
-    assert( Array.isArray( node.children ) )
+  it( 'creates a valid node value', () => {
+    const nodeType = 'someNodeType'
+    const nodeValue = Node.Value( nodeType )
+    assert.equal( typeof nodeValue._id, 'string' )
+    assert( nodeValue._id.length > 0 )
+    assert.equal( nodeValue.nodeType, nodeType )
+    assert( Object.keys( nodeValue ).length === 2 )
+  })
+  it( 'creates a valid node value with additional properties', () => {
+    const nodeType = 'someNodeType'
+    const nodeValue = Node.Value( nodeType, { extra: 21, name: 'sam' })
+    assert.equal( typeof nodeValue._id, 'string' )
+    assert( nodeValue._id.length > 0 )
+    assert.equal( nodeValue.nodeType, nodeType )
+    assert( Object.keys( nodeValue ).length === 4 )
+  })
+  it( 'creates a valid node value and overwrites the _id property', () => {
+    const nodeType = 'someNodeType'
+    const idOverwrite = 'specialId-1234'
+    const nodeValue = Node.Value( nodeType, { _id: idOverwrite })
+    assert( nodeValue._id === idOverwrite )
+    assert.equal( nodeValue.nodeType, nodeType )
+    assert( Object.keys( nodeValue ).length === 2 )
+  })
+  it( 'creates a valid node', () => {
+    const nodeType = 'someNodeType'
+    const node = Node( nodeType )
     assert.equal( typeof node.value._id, 'string' )
     assert( node.value._id.length > 0 )
+    assert.equal( node.value.nodeType, nodeType )
+    assert( Array.isArray( node.children ) )
+  })
+  it( 'returns instance of mtype.api referencing schema', () => {
+    const mtype = Node.mtype
+    const types = mtype.types()
+    console.log( types )
+
+    assert( types.length == 4 )
+    assert( types[ 3 ] === 'parentNode' )
   })
 })
